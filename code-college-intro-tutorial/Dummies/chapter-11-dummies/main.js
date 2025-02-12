684a3897c600d267552a643dde9db0090282e4fd
Ronny2001789 <ronaldmputla25@gmail.com>
2025-02-12 08:44:51 +0200

    x

---
+++ code-college-intro-tutorial/Dummies/chapter-11-dummies/main.js
@@ -0,0 +1,16 @@
+var family = ["ronny","given","maria"];
+var soccerplayer = ["dybala","messi"];
+var animals = ["dog",3,"cat"];
+var family = soccerplayer.concat(animals);  
+
+var dog = "bobby";
+var cat = "snoppy";
+var pets =[dog,cat];
+
+var mediumarray = [1,2,3];
+mediumarray[1] ="two"
+pets[1]="ronny"
+
+let array =[1,2,3,4,5,6,7,8,9]; 
+
+let smallArray = array.slice(0,3);
