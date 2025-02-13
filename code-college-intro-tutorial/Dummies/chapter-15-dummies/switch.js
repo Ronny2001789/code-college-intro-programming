6c09e34201a09fd78f31923c38b3e628531a257e
Ronny2001789 <ronaldmputla25@gmail.com>
2025-02-13 23:29:11 +0200

    bfy

---
+++ code-college-intro-tutorial/Dummies/chapter-15-dummies/switch.js
@@ -0,0 +1,63 @@
+//Listing 15.1
+
+// var myNumber = prompt("Enter your favorite day of the week!").toLowerCase();
+// var theResponse;
+
+// switch (myNumber) {
+//     case "monday":
+//         theResponse = "Ack!";
+//         break;
+//     case "tuesday":
+//         theResponse = "Taco day!";
+//         break;
+//     case "wednesday":
+//         theResponse = "Halfway there!";
+//         break;
+//     case "thursday":
+//         theResponse = "It's the new Friday!";
+//         break;
+//     case "friday":
+//         theResponse = "TGIF! Yeah!";
+//         break;
+//     case "saturday":
+//         theResponse = "What a day!";
+//         break;
+//     case "sunday":
+//         theResponse = "Sunday = Funday!";
+//         break;
+//     default:
+//         theResponse = "I haven't heard of that one!";
+//         break;
+// }
+
+// alert(theResponse);
+
+var myCar = prompt("enter your favorite car").toLowerCase();
+var theResponse;
+
+
+switch(myCar) {
+    case "bmw":
+         theResponse = "good car";
+         break;
+    case "benz":
+         theResponse = "awesome";
+         break;
+    case "toyota":
+         theResponse = "fantastic";
+         break;
+    case "kia":
+         theResponse = "picked good car";
+         break;
+     case "porche":
+         theResponse = "the exhausted car";
+         break;
+     default:
+        theResponse = "that is part of good cars ";
+        break;
+
+};         
+
+alert(theResponse);
+
+
--- code-college-intro-tutorial/my web page/main.js
+++ code-college-intro-tutorial/my web page/main.js
@@ -1,26 +1,38 @@
+document.getElementById("placeOrder").addEventListener("click", placeOrder );
+
+
+function placeOrder () {
+    document.getElementById("enter fullname").value;
+    document.getElementById("enter your location").value; 
+    document.getElementById("enter street number").value; 
+    document.getElementById("enter housenumber").value;
+    document.getElementById("enter your favorite fruit").value; 
+    document.getElementById("enter your favorite vegetable").value;
+
+};
+
+var work = 23; 
+
+
+if( work > 10) {    
 let name = prompt("who are you");
-let number =prompt("enter your password");
-var age =confirm("please read option carefully")
-
-
+    let number =prompt("enter your password");
+    var age =confirm("please read option carefully")
+};
 
 if(name = "ronny" , number = 23)
     alert("welcome")
 else("you are not allowed")
 
-function countUp() {
-     for (let i = 0; i < 10; i++) {
-        alert("your order is pending");
-    }
-}
-     
 
-function replaceIt() {
-let enteryourfullname  = "<span class='replacement'>" + document.getElementById('enteryourfullname').value + '</span>';
-let enteryourlocation  = "<span class='replacement'>" + document.getElementById('verb').value + '</span>';
-let enterstreetnumber = "<span class='replacement'>" + document.getElementById('verb').value + '</span>';
-let enterhousenumber = "<span class='replacement'>" + document.getElementById('verb').value + '</span>';
-let enteryourfavoritefruit = "<span class='replacement'>" + document.getElementById('verb').value + '</span>';
-let enteryourfavoritevegetable = "<span class='replacement'>" + document.getElementById('verb').value + '</span>';
-let doyouwantplastic? = "<span class='replacement'>" + document.getElementById('verb').value + '</span>';
-let willyoulikefreeitemsonyourbirthday? = "<span class='replacement'>" + document.getElementById('verb').value + '</span>';
+// function countUp() {
+//      for (let i = 0; i < 10; i++) {
+//         alert("your order is pending");
+//     }
+// }
+
+
+
+
+
+
